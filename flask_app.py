fe94c98007133e53966de31d5281b67dfa48d1fe
Shivam Behl <shivambehl123@gmail.com>
2020-05-30 17:18:47 +0530

    Design Updates

--- flask_app.py
+++ flask_app.py
@@ -37,5 +37,9 @@ def index():
     return render_template("index.html", errors=errors, results=results)
 
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
